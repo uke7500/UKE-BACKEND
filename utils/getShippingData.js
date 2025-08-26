52339e812d348c3f289820a2637b42632c8241c5
uke7500 <uke7500@gmail.com>
2025-08-26 18:56:46 +0530

    Update getShippingData.js

--- utils/getShippingData.js
+++ utils/getShippingData.js
@@ -1,26 +1,26 @@
-const getShippingData = async (orderID) => {
-    const shipping_data = await fetch("https://uke-strapi.onrender.com/api/shippings");
-    const shipping_data_json = await shipping_data.json();
-    console.log("All shippings from Strapi:", shipping_data_json);
+const getShippingData = async (shippingDocumentId) => {
+    // const shipping_data = await fetch("https://uke-strapi.onrender.com/api/shippings");
+    // const shipping_data_json = await shipping_data.json();
+    // console.log("All shippings from Strapi:", shipping_data_json);
 
-    // Try to match order_id with PayPal's orderID
-    const shipping_info = shipping_data_json.data.find(
-        (info) => info.order_id == orderID
-    );
+    // // Try to match order_id with PayPal's orderID
+    // const shipping_info = shipping_data_json.data.find(
+    //     (info) => info.order_id == orderID
+    // );
 
-    if (!shipping_info) {
-        throw new Error(`No shipping info found for orderID: ${orderID}`);
-    }
+    // if (!shipping_info) {
+    //     throw new Error(`No shipping info found for orderID: ${orderID}`);
+    // }
 
-    console.log("Matched shipping info:", shipping_info);
+    // console.log("Matched shipping info:", shipping_info);
 
-    const documentId = shipping_info.documentId;
-    if (!documentId) {
-        throw new Error(`documentId missing for orderID: ${orderID}`);
-    }
+    // const documentId = shipping_info.documentId;
+    // if (!documentId) {
+    //     throw new Error(`documentId missing for orderID: ${orderID}`);
+    // }
 
     const temp_buyer_shipping_info = await fetch(
-        `https://uke-strapi.onrender.com/api/shippings/${documentId}`
+        `https://uke-strapi.onrender.com/api/shippings/${shippingDocumentId}`
     );
     const buyer_shipping_info_json = await temp_buyer_shipping_info.json();
 
