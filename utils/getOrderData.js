dbe2321659ac90968dbedc76fefb015210c52a79
uke7500 <uke7500@gmail.com>
2025-08-26 19:24:44 +0530

    Update getOrderData.js

--- utils/getOrderData.js
+++ utils/getOrderData.js
@@ -1,6 +1,9 @@
 const getOrderData = async (documentId) => {
+
+    const STRAPI_URL = process.env.STRAPI_URL;
+
     const data = await fetch(
-        `https://uke-strapi.onrender.com/api/orders?filters[documentId][$eq]=${documentId}&populate[order_item][populate][product][populate]=*`
+        STRAPI_URL + `/api/orders?filters[documentId][$eq]=${documentId}&populate[order_item][populate][product][populate]=*`
     );
 
     const json = await data.json();
